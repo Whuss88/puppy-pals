df943bbf9319eb690c3bda0d9f668668e117b368
Whuss88 <williamhussey95@gmail.com>
2024-10-28 20:21:08 -0400

    app & main jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { App } from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = rootElement._reactRootContainer || createRoot(rootElement);
+if (!rootElement._reactRootContainer) {
+  rootElement._reactRootContainer = root;
+}
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
